398d144f18de56ff49c4e3a498b5498ad3759af4
raznara <raznara@s09-prof>
2020-10-23 20:34:04 +0200

    Initial commit

---
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/SessionBean.java
@@ -0,0 +1,23 @@
+package net.ausiasmarch.trolleyesSBserver.api;
+
+public class SessionBean {
+    private String Saludo;
+    private Integer Codigo;
+
+    public String getSaludo() {
+        return Saludo;
+    }
+
+    public void setSaludo(String Saludo) {
+        this.Saludo = Saludo;
+    }
+
+    public Integer getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(Integer Codigo) {
+        this.Codigo = Codigo;
+    }
+
+}
