1435574c7075828116c5351d56177f29ee43b21a
RAFAEL AZNAR <rafaaznar@gmail.com>
2020-10-29 17:19:18 +0100

    filtro CORS y acabar session

--- src/main/java/net/ausiasmarch/trolleyesSBserver/TrolleyesSBserverApplication.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/TrolleyesSBserverApplication.java
@@ -1,13 +1,40 @@
 package net.ausiasmarch.trolleyesSBserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+*/
 
 @SpringBootApplication
 public class TrolleyesSBserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrolleyesSBserverApplication.class, args);
-	}
+    //@Autowired
+    //private Environment env;
 
+    public static void main(String[] args) {
+        SpringApplication.run(TrolleyesSBserverApplication.class, args);
+    }
+     /*
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                String urls = env.getProperty("cors.urls");
+                CorsRegistration reg = registry.addMapping("/api/**");
+                for (String url : urls.split(",")) {
+                    reg.allowedOrigins(url);
+                }
+            }
+        };
+    }
+    */
 }
--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/SessionController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/SessionController.java
@@ -14,64 +14,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "http://localhost:8080/trolleyes", maxAge = 3600)
+//@CrossOrigin(origins = "*")
 @RequestMapping("/session")
 public class SessionController {
 
     @Autowired
     HttpSession oHttpSession;
 
+    @GetMapping("/private")
+    public ResponseEntity<?> secret() {
+        SessionBean oSessionBean = (SessionBean) oHttpSession.getAttribute("usuario");
+        if (oSessionBean == null) {
+            SessionBean oSessionBean2 = new SessionBean();
+            oSessionBean2.setMessage("NO SESSION");
+            oSessionBean2.setStatus(403);
+            return new ResponseEntity<SessionBean>(oSessionBean2, HttpStatus.OK);
+        } else {
+            SessionBean oSessionBean3 = new SessionBean();
+            oSessionBean3.setMessage("HOLA MUNDO SECRETO");
+            oSessionBean3.setStatus(200);
+            return new ResponseEntity<SessionBean>(oSessionBean3, HttpStatus.OK);
+        }
+    }
+
     @GetMapping("/")
     public ResponseEntity<?> check() {
         SessionBean oSessionBean = (SessionBean) oHttpSession.getAttribute("usuario");
         if (oSessionBean == null) {
             SessionBean oSessionBean2 = new SessionBean();
-            oSessionBean2.setMessage("NO SESION");
-            oSessionBean2.setStatus(200);
+            oSessionBean2.setMessage("NO SESSION");
+            oSessionBean2.setStatus(403);
             return new ResponseEntity<SessionBean>(oSessionBean2, HttpStatus.OK);
         } else {
             return new ResponseEntity<SessionBean>(oSessionBean, HttpStatus.OK);
         }
     }
 
-    /*
-    Queda pendiente...
     @PostMapping("/")
     public ResponseEntity<?> login(@RequestBody UsuarioBean oUsuarioBean) {
-     */
-    @GetMapping("/{login}/{password}")
-    public ResponseEntity<?> login(@PathVariable(value = "login") String strLogin, @PathVariable(value = "password") String strPassword) {
-
-        if (strLogin.equalsIgnoreCase("rafa")) {
-            if (strPassword.equalsIgnoreCase("79063e8037fff16d297a1fe65136f1251126cddb2cc9870ecf8d653835538e85")) {
+        if (oUsuarioBean.getLogin().equalsIgnoreCase("rafa")) {
+            if (oUsuarioBean.getPassword().equalsIgnoreCase("79063e8037fff16d297a1fe65136f1251126cddb2cc9870ecf8d653835538e85")) {
                 SessionBean oSessionBean = new SessionBean();
-                oSessionBean.setMessage(strLogin);
+                oSessionBean.setMessage(oUsuarioBean.getLogin());
                 oSessionBean.setStatus(200);
                 oHttpSession.setAttribute("usuario", oSessionBean);
                 return new ResponseEntity<SessionBean>(oSessionBean, HttpStatus.OK);
             }
         }
-        /*
-        if (oUsuarioBean.getLogin().equalsIgnoreCase("rafa")) {
-            if (oUsuarioBean.getPassword().equalsIgnoreCase("rafael")) {
-                SessionBean oSessionBean = new SessionBean();
-                oSessionBean.setMessage(oUsuarioBean.getLogin());
-                oSessionBean.setStatus(200);
-                return new ResponseEntity<SessionBean>(oSessionBean, HttpStatus.OK);
-            }
-        }
-         */
         SessionBean oSessionBean = new SessionBean();
         oSessionBean.setMessage("ERROR");
         oSessionBean.setStatus(500);
-        return new ResponseEntity<SessionBean>(oSessionBean, HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<SessionBean>(oSessionBean, HttpStatus.OK);
     }
 
     @DeleteMapping("/")
     public ResponseEntity<?> logout() {
         oHttpSession.invalidate();
         SessionBean oSessionBean = new SessionBean();
-        oSessionBean.setMessage("OK");
+        oSessionBean.setMessage("Bye");
         oSessionBean.setStatus(200);
         return new ResponseEntity<SessionBean>(oSessionBean, HttpStatus.OK);
     }
