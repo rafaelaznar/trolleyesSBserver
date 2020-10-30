ea4d39333450b6c1c84339f53dd3ba1818df58b8
Rafael <Rafael@DESKTOP-H171HCF>
2020-10-30 19:36:36 +0100

    Método GET de: TipousuarioController, TipousuarioEntity y TipousuarioRepository

---
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/TipousuarioController.java
@@ -0,0 +1,30 @@
+
+package net.ausiasmarch.trolleyesSBserver.api;
+
+import javax.servlet.http.HttpSession;
+import net.ausiasmarch.trolleyesSBserver.entity.TipousuarioEntity;
+import net.ausiasmarch.trolleyesSBserver.repository.TipousuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tipousuario")
+public class TipousuarioController {
+    
+    @Autowired
+    HttpSession oHttpSession;
+
+    @Autowired
+    TipousuarioRepository oTipousuarioRepository;
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable(value = "id") Long id) {
+        return new ResponseEntity<TipousuarioEntity>(oTipousuarioRepository.getOne(id), HttpStatus.OK);
+    }
+    
+}
