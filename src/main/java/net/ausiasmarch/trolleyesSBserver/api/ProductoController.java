24547ed956a273f3aae0d8d3c227f36a4e3076c8
RAFAEL AZNAR <rafaaznar@gmail.com>
2020-11-02 20:05:30 +0100

    producto create

--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/ProductoController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/ProductoController.java
@@ -15,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,6 +50,11 @@ public class ProductoController {
     @GetMapping("/count")
     public ResponseEntity<?> count() {
         return new ResponseEntity<Long>(oProductoRepository.count(), HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<?> create(@RequestBody ProductoEntity oProductoEntity) {
+        return new ResponseEntity<ProductoEntity>(oProductoRepository.save(oProductoEntity), HttpStatus.OK);
     }
 
 }
