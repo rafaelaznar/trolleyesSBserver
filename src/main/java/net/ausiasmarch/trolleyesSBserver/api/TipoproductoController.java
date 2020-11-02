71bc5ee40eda1a658387d0930131e72edad3db04
beapm <Bea@Bea>
2020-11-02 20:28:36 +0100

    Tipoproducto create

--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/TipoproductoController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/TipoproductoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,12 @@ public class TipoproductoController {
     @GetMapping("/count")
     public ResponseEntity<?> count() {
         return new ResponseEntity<Long>(oTipoproductoRepository.count(), HttpStatus.OK);
+    }
+    
+    @PostMapping("/")
+    public ResponseEntity<?> create (@RequestBody TipoproductoEntity oTipoproductoEntity) {
+                return new ResponseEntity<TipoproductoEntity>(oTipoproductoRepository.save(oTipoproductoEntity), HttpStatus.OK);
+
     }
 
 }
