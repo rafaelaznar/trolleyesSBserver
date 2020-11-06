e9c928b6dd5b20964a0df8cb48d5c3b39c69419c
Oscar Garcia <oscgarciaescriva@gmail.com>
2020-11-06 20:16:45 +0100

    + tipoproducto delete

--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/TipoproductoController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/TipoproductoController.java
@@ -9,6 +9,7 @@ import net.ausiasmarch.trolleyesSBserver.service.FillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,5 +67,14 @@ public class TipoproductoController {
         return new ResponseEntity<ResponseBean>(oSessionBean, HttpStatus.OK);
 
     }
-
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+        oTipoproductoRepository.deleteById(id);
+        if (oTipoproductoRepository.existsById(id)) {
+            return new ResponseEntity<Long>(id, HttpStatus.NOT_MODIFIED);
+        } else {
+            return new ResponseEntity<Long>(0L, HttpStatus.OK);
+        }
+    }
 }
