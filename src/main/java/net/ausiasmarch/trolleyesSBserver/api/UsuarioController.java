a83bdbd89befac9375b6870695b40ae04679b904
eugenio.prieto <euxeni911@gmail.com>
2020-11-07 11:07:04 +0100

    GET Usuario (Faltaba añadir de la tarea anterior) GET Tipoproducto (retocar)

--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/UsuarioController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/UsuarioController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,6 +50,15 @@ public class UsuarioController {
     @GetMapping("/count")
     public ResponseEntity<?> count() {
         return new ResponseEntity<Long>(oUsuarioRepository.count(), HttpStatus.OK);
+    }
+    
+     @GetMapping("/{id}")
+    public ResponseEntity<?> get(@PathVariable(value = "id") Long id) {
+        if (oUsuarioRepository.existsById(id)) {
+            return new ResponseEntity<UsuarioEntity>(oUsuarioRepository.getOne(id), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<UsuarioEntity>(oUsuarioRepository.getOne(id), HttpStatus.NOT_FOUND);
+        }
     }
 
 }
