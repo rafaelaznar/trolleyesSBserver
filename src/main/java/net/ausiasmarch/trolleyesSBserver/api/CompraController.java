359a4047182085a6b7f157e36c2cc1c09a82534a
rafae <rafae@DESKTOP-IHEHRF8>
2020-11-16 19:52:09 +0100

    compra page by Rafa Montesinos

--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/CompraController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/CompraController.java
@@ -38,6 +38,10 @@ import net.ausiasmarch.trolleyesSBserver.entity.CompraEntity;
 import net.ausiasmarch.trolleyesSBserver.repository.CompraRepository;
 import net.ausiasmarch.trolleyesSBserver.service.FillService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -117,6 +121,12 @@ public class CompraController {
         } else {
             return new ResponseEntity<Long>(0L, HttpStatus.OK);
         }
+    }
+    
+    @GetMapping("/page")
+    public ResponseEntity<?> getPage(@PageableDefault(page = 0, size = 10, direction = Direction.ASC) Pageable oPageable) {
+        Page<CompraEntity> oPage = oCompraRepository.findAll(oPageable);
+        return new ResponseEntity<Page<CompraEntity>>(oPage, HttpStatus.OK);
     }
 
 }
