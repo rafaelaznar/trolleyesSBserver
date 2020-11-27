905c11775d03903765c49dce5b4aebd5c0739457
beapm <beapm1994@gmail.com>
2020-11-27 10:04:25 +0100

    compra count - seguridad - by Bea Penalba

--- src/main/java/net/ausiasmarch/trolleyesSBserver/api/CompraController.java
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/api/CompraController.java
@@ -112,7 +112,18 @@ public class CompraController {
 
     @GetMapping("/count")
     public ResponseEntity<?> count() {
-        return new ResponseEntity<Long>(oCompraRepository.count(), HttpStatus.OK);
+
+        UsuarioEntity oUsuarioEntity = (UsuarioEntity) oHttpSession.getAttribute("usuario");
+
+        if (oUsuarioEntity == null) {
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+        } else {
+            if (oUsuarioEntity.getTipousuario().getId() == 1) {
+                return new ResponseEntity<Long>(oCompraRepository.count(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            }
+        }
     }
 
     @PostMapping("/fill/{amount}")
