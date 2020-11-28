5286d115b9e70ebda57ea1775b96b324e1041394
Rafael Aznar <rafa.aznar@gmail.com>
2020-11-29 00:04:57 +0100

    product Images upload, store & download

---
+++ src/main/java/net/ausiasmarch/trolleyesSBserver/entity/FileEntity.java
@@ -0,0 +1,79 @@
+package net.ausiasmarch.trolleyesSBserver.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.awt.image.BufferedImage;
+import java.sql.Blob;
+
+@Entity
+@Table(name = "file")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class FileEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "type")
+    private String type;
+    //image bytes can have large lengths so we specify a value
+    //which is more than the default length for picByte column
+    @Column(name = "file")
+    //private byte[] file;
+    private Blob file;
+
+    public FileEntity() {
+    }
+
+    public FileEntity(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Blob getFile() {
+        return file;
+    }
+
+    public void setFile(Blob fileBlob) {
+        this.file = fileBlob;
+    }
+
+
+
+}
